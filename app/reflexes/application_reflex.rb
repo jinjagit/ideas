614e1fa168274f954a0662d98387d62f3b1e286f
Marco Roth <marco.roth@intergga.ch>
2021-06-19 23:29:57 +0200

    identify ActionCable connection with current_user

--- app/reflexes/application_reflex.rb
+++ app/reflexes/application_reflex.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationReflex < StimulusReflex::Reflex
-  # Put application-wide Reflex behavior and callbacks in this file. 
-  #
-  # Example:
-  #
-  #   # If your ActionCable connection is: `identified_by :current_user`
-  #   delegate :current_user, to: :connection
+  delegate :current_user, to: :connection
+
+  # Put application-wide Reflex behavior and callbacks in this file.
   #
   # Learn more at: https://docs.stimulusreflex.com/reflexes#reflex-classes
 end
