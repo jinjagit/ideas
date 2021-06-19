1c39fabcab968e6bf01c0526bd121ce327199378
Simon Tharby <simon@liquidvoting.io>
2021-06-19 20:52:41 +0100

    Show delete button on comments for current user, and make this work

--- app/reflexes/comments_reflex.rb
+++ app/reflexes/comments_reflex.rb
@@ -2,4 +2,10 @@ class CommentsReflex < ApplicationReflex
   def add
     Idea.find(params[:id]).comments.create(body: params[:body], username: params[:username])
   end
+
+  def delete
+    id = element.dataset["comment-id"]
+    comment = Idea.find(params[:id]).comments.find(id)
+    comment.destroy
+  end
 end
