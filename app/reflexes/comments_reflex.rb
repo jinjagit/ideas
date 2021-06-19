90bed6e5304e740a0bf65123c437c559099ddc25
Simon Tharby <simon@liquidvoting.io>
2021-06-19 20:08:55 +0100

    Use current_user email as username when create new comment
    Also display comment username on each comment

--- app/reflexes/comments_reflex.rb
+++ app/reflexes/comments_reflex.rb
@@ -1,5 +1,5 @@
 class CommentsReflex < ApplicationReflex
   def add
-    Idea.find(params[:id]).comments.create(body: params[:body])
+    Idea.find(params[:id]).comments.create(body: params[:body], username: params[:username])
   end
 end
