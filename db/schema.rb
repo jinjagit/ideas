f015d060d83533caa830465054600c040279e367
Simon Tharby <simon@liquidvoting.io>
2021-06-19 17:30:03 +0100

    Add reflex controller for comment add action
    Also add comments list & form to idea show view
    Also set @comments instance var in idea show def

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_19_160408) do
+ActiveRecord::Schema.define(version: 2021_06_19_162226) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "ideas", force: :cascade do |t|
     t.string "name"
